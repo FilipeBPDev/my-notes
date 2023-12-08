a952dc4e107fd02c801130500997392bbe0df198
Filipe <devfilipebp@gmail.com>
2023-12-08 13:09:27 -0300

    :sparkles: Update note in database

--- src/components/pages/NoteProject/NoteProject.js
+++ src/components/pages/NoteProject/NoteProject.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
 
@@ -7,68 +7,91 @@ import Button from "../../layout/Button/Button";
 
 function NoteProject() {
     const { id } = useParams();
-    const [title, setTitle] = useState('')
-    const [description, setDescription] = useState('')
-    const [editTitle, setEditTitle] = useState(false)
-    const [editDescription, setEditDescription] = useState(false)
+    const containerRef = useRef(null);
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+    const [editTitle, setEditTitle] = useState(false);
+    const [editDescription, setEditDescription] = useState(false);
 
     useEffect(() => {
         axios.get(`http://localhost:7007/notes/${id}`)
             .then(res => {
                 const { title, description } = res.data;
-                setTitle(title)
-                setDescription(description)
+                setTitle(title);
+                setDescription(description);
             })
-            .catch(err => console.log(err))
-    }, [id])
+            .catch(err => console.log(err));
+    }, [id]);
 
+    const handleContainerClick = () => {
+        setEditTitle(true);
+        setEditDescription(true);
+    };
+
+    const handleOutsideClick = (e) => {
+        if (containerRef.current && !containerRef.current.contains(e.target)) {
+            setEditTitle(false);
+            setEditDescription(false);
+        }
+    };
+
+    useEffect(() => {
+        document.addEventListener("mousedown", handleOutsideClick);
+        return () => {
+            document.removeEventListener("mousedown", handleOutsideClick);
+        };
+    }, []);
+
+    const updateNote = () => {
+        axios.patch(`http://localhost:7007/notes/${id}`, {
+            title,
+            description,
+        })
+            .then(res => {
+                console.log("Nota atualizada com sucesso", res.data)
+                setEditTitle(false);
+                setEditDescription(false);
+            })
+
+            .catch(err => {
+                console.log("Erro ao atualizar nota.", err)
+            })
+
+    }
 
     return (
-        <div className={styles.container}>
-
-            <div className={styles.title} onClick={() => setEditTitle(true)}>
+        <div className={styles.container} onClick={handleContainerClick} ref={containerRef}>
+            <div className={styles.title}>
                 {editTitle ? (
                     <input
                         type="text"
                         value={title}
                         onChange={(e) => setTitle(e.target.value)}
-                        onBlur={() => setEditTitle(false)}
                         autoFocus
                     />
                 ) : (
                     <h1>{title}</h1>
                 )}
             </div>
-
-            <div className={styles.description} onClick={() => setEditDescription(true)}>
+            <div className={styles.description}>
                 {editDescription ? (
                     <textarea
                         value={description}
                         onChange={(e) => setDescription(e.target.value)}
-                        onBlur={() => setEditDescription(false)}
                         autoFocus
                     />
-
                 ) : (
                     <p>{description}</p>
                 )}
                 {(editTitle || editDescription) && (
                     <div className={styles.divButton}>
-                        <Button
-                            text="Salvar"
-                        />
+                        <Button text="Salvar"
+                            handleSubmit={updateNote} />
                     </div>
-
                 )}
-
-
             </div>
-
-
-
         </div>
-    )
+    );
 }
 
 export default NoteProject;
-
